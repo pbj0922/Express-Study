5495c94c4e0b2b89fa6b079b5c2f7468fd0d3709
Bumjin Park <qkrqjawls0922@gmail.com>
2023-09-26 08:16:27 +0900

    add prisma & connect planetscale

--- index.ts
+++ index.ts
@@ -1,28 +1,66 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3010;
 
+const client = new PrismaClient();
+
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  return res.send("Hello, Express!");
+  return res.json("Hello, Express!");
 });
 
-app.get("/my-page", (req, res) => {
-  return res.json({
-    ok: true,
-    message: "This is my page.",
-  });
-});
+app.post("/user", async (req, res) => {
+  try {
+    const { account, password } = req.body;
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+    // 유저가 존재하는지 확인
+    const existUser = await client.user.findUnique({
+      where: {
+        account,
+      },
+    });
 
-  return res.json({
-    ok: true,
-  });
+    // 있으면 리턴(종료)
+    if (existUser) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Already exist user." });
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    // console.log(password);
+    // console.log(hashedPassword);
+    // console.log(bcrypt.compareSync(password + "1", hashedPassword));
+
+    // 없으면 생성
+    const newUser = await client.user.create({
+      data: {
+        account,
+        password: hashedPassword,
+      },
+    });
+
+    // 생성후 종료
+    console.log(newUser);
+
+    return res.json({
+      ok: true,
+      user: {
+        id: newUser.id,
+        createdAt: newUser.createdAt,
+        updatedAt: newUser.updatedAt,
+        account: newUser.account,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(port, () => {
